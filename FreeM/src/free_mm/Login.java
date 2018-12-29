cdc4be1b05349f1122155af9b397c19a2ceba2f3
watanabe_kazuki <tomoki.watanabe.likeit@gmail.com>
2018-12-30 03:12:28 +0900

    12/29

--- FreeM/src/free_mm/Login.java
+++ FreeM/src/free_mm/Login.java
@@ -1,12 +1,18 @@
 package free_mm;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.UserDateBeans;
+import dao.UserDao;
 
 /**
  * Servlet implementation class Login
@@ -15,13 +21,13 @@ import javax.servlet.http.HttpServletResponse;
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -38,7 +44,40 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		// リクエストパラメータの文字コードを指定
+		request.setCharacterEncoding("UTF-8");
 
+		String loginId = request.getParameter("loginId");
+		String password = request.getParameter("password");
+		try {
+			// リクエストパラメータの入力項目を引数に渡して、Daoのメソッドを実行
+			UserDao userDao = new UserDao();
+			int userId = userDao.getUserId(loginId, password);
+
+			UserDateBeans user = UserDao.UserDateBeans(userId);
+
+			/** テーブルに該当のデータが見つからなかった場合 **/
+			if (user == null) {
+				// リクエストスコープにエラーメッセージをセット
+				request.setAttribute("errMsg", "ログインIDまたはパスワードが異なります");
+
+				// ログインjspにフォワード
+				request.getRequestDispatcher(FMHelper.LOGIN_PAGE).forward(request, response);
+			return;
+			}
+			/** テーブルに該当のデータが見つかった場合 **/
+			// セッションにユーザの情報をセット
+			HttpSession session = request.getSession();
+			session.setAttribute("userInfo", user);
+
+			// ユーザ一覧のサーブレットにリダイレクト
+			response.sendRedirect("Index");
+
+
+		}catch(SQLException | NoSuchAlgorithmException e){
+			e.printStackTrace();
+
+		}
+
+	}
 }
